03a91549b0811431dc57c91d71eb8b5c4852ebb4
Daniel <d0583258844@gmail.com>
2025-11-10 11:00:02 +0200

    created the class book

---
+++ book.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self, title, author, ISBN, is_available : bool):
+        self. title = title
+        self.author = author
+        self.ISBN = ISBN
+        self.is_available = is_available
+
+    
+    def __str__(self):
+        return f"your choice: {self.title} by:{self.author} status: {self.is_available}"
+    
+
+    
